0c66e9a40551e66d289d00544d5995504b3f7289
donaldoempire1999 <donaldfotso1999@gmail.com>
2021-06-08 02:51:28 +0100

    Full Integration

--- ontoFoodApp/ontoHome/views.py
+++ ontoFoodApp/ontoHome/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from ontoFoodApp.onto_class import OntoFoodDao
 
 # Create your views here.
 class Home(View):
     def get(self, request):
         return render(request=request , template_name='ontoHome/index.html')
+
+
+
+
+"""class TestView(View):
+    def get(self , request):
+        # ontoDao = OntoFoodDao.get_all_sauces()"""
